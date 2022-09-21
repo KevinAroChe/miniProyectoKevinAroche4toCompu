a4fc8eaad63a663be5229ef8301f69d78fa364fc
unknown <Juarez.2005.Kevin@gmail.com>
2022-09-21 15:57:37 -0600

    funciones crear buscar uno y todos, modificar

--- Inicio.py
+++ Inicio.py
@@ -1,9 +1,56 @@
 from persona import persona
 misContactos = []
 
-def crearContacto():
-    print("Creando contacto")
+def crearContacto(numero, nombre, direccion):
+    misContactos.append(persona(numero, nombre, direccion))
+    print("Contato almacenado")
 
+def buscarContactos(nombre):
+    if len(misContactos) == 0:
+       print("La lista esta vacia, no  hay contatos que buscar")
+    else:
+        encontrado = False
+        for i in range(len(misContactos)):
+            if misContactos[i].verNombre() == nombre:
+               print("El telefono es", misContactos[i].verNumero())
+               print("La direccion es ", misContactos[i].verDireccion())
+               encontrado = True
+               break
+            else:
+                encontrado - False
+        if encontrado == False:
+         print("Dato no existente..")
+
+def mostrarContactos():
+    if len(misContactos) == 0:
+        print("La lista esta vacía, no hay contactos que buscar...")
+    else:
+        for i in range(len(misContactos)):
+            print('Nombre: ', misContactos[i].verNombre(), ' Dirección', misContactos[i].verDireccion(), ' Teléfono', misContactos[i].verNumero())
+
+def modificarContacto(nombre):
+    if len(misContactos) == 0:
+        print("La lista esta vacía, no hay contactos que buscar...")
+    else:
+        encontrado = False
+        posicion = None
+        for i in range(len(misContactos)):
+            if misContactos[i].verNombre() == nombre:
+                posicion = i
+                encontrado = True
+                break
+            else:
+                encontrado = False
+        if encontrado:
+            nuevoNumero = int(input("Ingrese el nuevo número: "))
+            nuevoNombre = input("Ingrese nuevo nombre: ")
+            nuevaDireccion = input("Ingrese la nueva dirección: ")
+            misContactos[posicion].modificarNumero(nuevoNumero)
+            misContactos[posicion].modificarNombre(nuevoNombre)
+            misContactos[posicion].modificarDireccion(nuevaDireccion)
+            print("Datos actualizados con éxito...")
+        else:
+            print("Dato no encontrado...")
 
 def main():
     op = 0
@@ -21,7 +68,10 @@ def main():
         numero = int(input("ingrse el numero de telefono"))
         nombre = int(input("ingrse el nombre:"))
         direccion = int(input("ingrse la direccion"))
-        crearContacto()
+        crearContacto(numero, nombre, direccion)
+    elif op ==2:
+        nombre = input("Ingrese el nombre del contacto a buscar ")
+        buscarContactos(nombre)
 
 #iniciar programa
 main()
--- persona.py
+++ persona.py
@@ -1,8 +1,28 @@
-from socket import NI_NUMERICHOST
-
-
 class persona():
     def __init__(self, numero, nombre, direccion):
         self.numero = numero
         self.nombre = nombre
         self.direccion = direccion
+
+
+    #creacion de GETTERS
+    def verNumero(self):
+        return self.numero
+
+    def verNombre(self):
+        return self.nombre
+
+    def verDireccion(self):
+        return self.direccion
+    
+    #Creación de SETTERS
+    def modificarNumero(self, nuevoNumero):
+        self.numero = nuevoNumero
+    
+    def modificarNombre(self, nuevoNombre):
+        self.nombre = nuevoNombre
+
+    def modificarDireccion(self, nuevaDireccion):
+        self.direccion = nuevaDireccion
+
+
